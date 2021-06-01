e6ae6b4f02d65eccc4a65eb229d447d8f28bd36b
1016281633@qq.com <1016281633@qq.com>
2021-06-01 19:50:51 +0800

    获取orderid 新增订单详情接口

--- axx_unittest/case/demo7.py
+++ axx_unittest/case/demo7.py
@@ -3,8 +3,11 @@
 # @Author : jiapeng
 # @File : demo7.py
 import unittest
+
+import jsonpath
 import requests
 import json
+import random
 
 class axx_learn(unittest.TestCase):
 
@@ -12,8 +15,9 @@ class axx_learn(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.ptpc=None
         cls.ptpcUserId=None
+        cls.orderId=None
 
-    def test_login(self):
+    def test_01_login(self):
         url='http://www.aixuexi.com/surrogates/passport/user/v2/login'
         data={
             "device": "23582a6aa449a38775e7f05b7941f97c",
@@ -32,7 +36,7 @@ class axx_learn(unittest.TestCase):
 
 
 #     订单列表
-    def test_pay_list(self):
+    def test_02_pay_list(self):
         header={
             'userid':self.ptpcUserId,
             'ptpc':self.ptpc
@@ -40,6 +44,22 @@ class axx_learn(unittest.TestCase):
         url='http://ghostrider.aixuexi.com/pop/order/pay-list?studentName=&orderNo=&phone=&goodsName=&orderStatus=&businessType=1&orderSource=&orderAccount=&pageNum=1&pageSize=10&startTime=2021-05-03%2000:00:00&endTime=2021-06-02%2023:59:59'
         r=requests.get(url=url,headers=header)
         print(json.dumps(r.json(),indent=2,ensure_ascii=False))
+        key = input('请输入要获取的key:')
+        axx_learn.orderId=jsonpath.jsonpath(r.json(),'$..{0}'.format(key))
+        print(axx_learn.orderId)
+
+#     订单详情
+
+    def test_03_order_detail(self):
+        header = {
+            'userid': self.ptpcUserId,
+            'ptpc': self.ptpc
+        }
+        orderId=random.choice(self.orderId)
+        print(orderId)
+        url='http://ghostrider.aixuexi.com/pop/order/NCTS/pay-order-detail?orderId='+str(orderId)
+        r=requests.get(url=url,headers=header)
+        print(json.dumps(r.json(), indent=2, ensure_ascii=False))
 
 if __name__ == '__main__':
     unittest.main()
